155c06c82e73bd85fec6de434bd2d87d77919a5e
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2023-08-20 10:01:26 +0900

    init

---
+++ lib/octovy/test.rb
@@ -0,0 +1,8 @@
+require "octovy/test/version"
+
+module Octovy
+  module Test
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/octovy/test/version.rb
@@ -0,0 +1,5 @@
+module Octovy
+  module Test
+    VERSION = "0.1.0"
+  end
+end
